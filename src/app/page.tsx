26069cc33598c5493302c86f74091a86f4476337
Nishi <nishishah0104@gmail.com>
2024-09-16 18:16:39 +0530

    'change'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -225,7 +225,8 @@ const Home = () => {
 
 
 
-  <footer className="bg-gray-900 text-white py-12">
+  <footer className="bg-gradient-to-r from-[#056F92] to-[#063954] text-white py-12">
+
   <div className="max-w-screen-lg mx-auto flex flex-col md:flex-row justify-between items-start space-y-10 md:space-y-0 gap-11">
    
     <div className="text-center md:text-left">
@@ -288,7 +289,7 @@ const Home = () => {
   </div>
 
 
-  <div className="mt-8 text-center text-gray-500 border-t border-gray-700 pt-4">
+  <div className="mt-8 text-center text-white border-t border-gray-700 pt-4">
     <p>© sprect</p>
     <p className="text-sm">made with ❤️ in India, for the world.</p>
     <span className="[&>svg]:h-5 [&>svg]:w-5 text-right">
@@ -314,6 +315,18 @@ const Home = () => {
         >
           <path
             d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"
+          />
+        </svg>
+      </a>
+      <a href="https://www.facebook.com" aria-label="facebook">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 448 512"
+          className="h-5 w-5"
+        >
+          <path
+            d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"
           />
         </svg>
       </a>
